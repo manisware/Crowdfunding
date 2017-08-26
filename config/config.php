b0084a077064dafa5a75626678b2be1f326ea0f3
manisware <info@manisware.com>
2017-08-27 04:32:39 +0530

    Create config.php
    Database Configuration

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+# In "config.php"
+# DB configuration
+define('DB_NAME', 'crowdfunding');
+define('DB_USER', 'USERNAME');
+define('DB_PASSWORD', 'PASSWORD');
+define('DB_HOST', 'localhost');
+
+?>
